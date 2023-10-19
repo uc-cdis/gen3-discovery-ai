707421b0f3d6ecd92fb1b00277b8e2d2e27c6b13
Alex VanTol <avantol@uchicago.edu>
2023-10-19 14:15:11 -0500

    fix(ci): use poetry run for pytest script

--- tests/ci_commands_script.sh
+++ tests/ci_commands_script.sh
@@ -3,4 +3,4 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.." || fail
-pytest -vv --cov-config=.coveragerc --cov=gen3discoveryai --cov-report term-missing:skip-covered --cov-fail-under 95 --cov-report html:_coverage --cov-branch
+poetry run pytest -vv --cov-config=.coveragerc --cov=gen3discoveryai --cov-report term-missing:skip-covered --cov-fail-under 95 --cov-report html:_coverage --cov-branch
