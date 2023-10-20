d42b57dd1392baa1635c80d77a933daa515aa720
Alex VanTol <avantol@uchicago.edu>
2023-10-20 15:47:47 -0500

    chore(lint): cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,15 +4,15 @@ import os
 import pytest
 from starlette.testclient import TestClient
 
+from gen3discoveryai import config
+from gen3discoveryai.main import get_app
+
 
 @pytest.fixture()
 def client():
-    from gen3discoveryai import config
-    from gen3discoveryai.main import get_app
-
     # change dir to the tests, so it loads the test .env
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     importlib.reload(config)
 
-    with TestClient(get_app()) as client:
-        yield client
+    with TestClient(get_app()) as test_client:
+        yield test_client
