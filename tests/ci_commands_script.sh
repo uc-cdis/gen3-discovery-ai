9d33460263e68e09aad4f1e5648cd7524805144b
Alex VanTol <avantol@uchicago.edu>
2024-02-05 15:11:36 -0600

    fix(tests): fix config import error by adjusting test env setup instead of modifying tests

--- tests/ci_commands_script.sh
+++ tests/ci_commands_script.sh
@@ -6,7 +6,7 @@ cd "$SCRIPT_DIR/.." || fail
 poetry env info
 
 echo "current directory: $(pwd)"
-echo "moving the test configuration .env to be the default config for the app w/ `cp tests/.env ../.env`"
-cp tests/.env ../.env
+echo "moving the test configuration .env to be the default config for the app w/ 'cp tests/.env ../.env'"
+cp tests/.env .env
 
 poetry run pytest -vv --cov-config=.coveragerc --cov=gen3discoveryai --cov-report term-missing:skip-covered --cov-fail-under 90 --cov-report html:_coverage --cov-branch
